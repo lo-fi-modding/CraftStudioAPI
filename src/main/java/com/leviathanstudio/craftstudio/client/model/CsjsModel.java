bf137676907e4805951ec921846a404c623605bd
Corey Frenette <lordmonoxide@gmail.com>
2022-01-08 21:39:29 -0400

    Models loading

--- src/main/java/com/leviathanstudio/craftstudio/client/model/CsjsModel.java
+++ src/main/java/com/leviathanstudio/craftstudio/client/model/CsjsModel.java
@@ -8,12 +8,14 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.mojang.blaze3d.vertex.VertexFormatElement;
+import com.mojang.datafixers.util.Pair;
 import com.mojang.math.Vector3f;
 import com.mojang.math.Vector4f;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.ModelState;
+import net.minecraft.client.resources.model.UnbakedModel;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Vec3i;
 import net.minecraft.resources.ResourceLocation;
@@ -27,28 +29,28 @@ import net.minecraftforge.client.model.pipeline.BakedQuadBuilder;
 import net.minecraftforge.client.model.pipeline.IVertexConsumer;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 
 public class CsjsModel implements IMultipartModelGeometry<CsjsModel> {
   public final String title;
   private final Map<String, Part> parts;
-  private final List<Part> flatParts;
+  private final Map<String, Part> flatParts;
 
   public CsjsModel(final String title, final Map<String, Part> parts) {
     this.title = title;
     this.parts = parts;
-    this.flatParts = this.flattenParts(new ArrayList<>(), this.parts);
+    this.flatParts = this.flattenParts(new HashMap<>(), this.parts);
   }
 
-  private List<Part> flattenParts(final List<Part> output, final Map<String, Part> parts) {
-    output.addAll(parts.values());
+  private Map<String, Part> flattenParts(final Map<String, Part> output, final Map<String, Part> parts) {
+    output.putAll(parts);
 
     for(final Part part : parts.values()) {
       this.flattenParts(output, part.children);
@@ -59,17 +61,19 @@ public class CsjsModel implements IMultipartModelGeometry<CsjsModel> {
 
   @Override
   public Collection<? extends IModelGeometryPart> getParts() {
-    return this.flatParts;
+    return this.flatParts.values();
   }
 
   @Override
   public Optional<? extends IModelGeometryPart> getPart(final String name) {
-    return Optional.empty();
+    return Optional.ofNullable(this.flatParts.get(name));
   }
 
   public static class Part implements IModelGeometryPart {
     public final String name;
     public final Map<String, Part> children;
+
+    public final Vector3f parentPos;
 
     public final Vector3f pos;
     public final Vector3f offset;
@@ -77,9 +81,10 @@ public class CsjsModel implements IMultipartModelGeometry<CsjsModel> {
     public final Vector3f rotation;
     public final Vector3f uv; // Only uses (x, y)
 
-    public Part(final String name, final Map<String, Part> children, final Vector3f pos, final Vector3f offset, final Vector3f size, final Vector3f rotation, final Vector3f uv) {
+    public Part(final String name, final Map<String, Part> children, final Vector3f parentPos, final Vector3f pos, final Vector3f offset, final Vector3f size, final Vector3f rotation, final Vector3f uv) {
       this.name = name;
       this.children = children;
+      this.parentPos = parentPos;
       this.pos = pos;
       this.offset = offset;
       this.size = size;
@@ -94,109 +99,98 @@ public class CsjsModel implements IMultipartModelGeometry<CsjsModel> {
 
     @Override
     public void addQuads(final IModelConfiguration owner, final IModelBuilder<?> modelBuilder, final ModelBakery bakery, final Function<Material, TextureAtlasSprite> spriteGetter, final ModelState modelTransform, final ResourceLocation modelLocation) {
-      //TODO this might be getting called multiple times for children since the child parts are also part of the main model (see flattenParts etc above)
-
       final TextureAtlasSprite sprite = spriteGetter.apply(owner.resolveTexture("texture"));
 
-      this.buildPart(modelBuilder, sprite, this, 0.0f, 0.0f, 0.0f);
+      this.buildPart(modelBuilder, sprite, this);
     }
 
-    private void buildPart(final IModelBuilder<?> modelBuilder, final TextureAtlasSprite sprite, final Part part, final float offsetX, final float offsetY, final float offsetZ) {
-      //TODO centre point
-      final float originX = 0.0f;
-      final float originY = 0.0f;
-      final float originZ = 0.0f;
+    @Override
+    public Collection<Material> getTextures(IModelConfiguration owner, Function<ResourceLocation, UnbakedModel> modelGetter, Set<Pair<String, String>> missingTextureErrors) {
+      return Collections.singletonList(owner.resolveTexture("texture"));
+    }
 
-      final float x = offsetX + part.pos.x() + originX;
-      final float y = offsetY + part.pos.y() + originY;
-      final float z = offsetZ + part.pos.z() + originZ;
+    private void buildPart(final IModelBuilder<?> modelBuilder, final TextureAtlasSprite sprite, final Part part) {
+      //TODO centre point
+      final float originX = 8.0f - part.size.x() / 2.0f;
+      final float originY =      - part.size.y() / 2.0f;
+      final float originZ = 8.0f - part.size.z() / 2.0f;
+
+      final float x = this.parentPos.x() + part.pos.x() + originX + this.offset.x();
+      final float y = this.parentPos.y() + part.pos.y() + originY + this.offset.y();
+      final float z = this.parentPos.z() + part.pos.z() + originZ + this.offset.z();
 
       final float scale = 16.0f;
+      final float texScale = 64.0f;
 
       final float x0 = x / scale;
-      final float x1 = (x + part.size.x()) / scale;
       final float y0 = y / scale;
-      final float y1 = (y + part.size.y()) / scale;
       final float z0 = z / scale;
+      final float x1 = (x + part.size.x()) / scale;
+      final float y1 = (y + part.size.y()) / scale;
       final float z1 = (z + part.size.z()) / scale;
+
+      final float u0 = part.uv.x() / texScale;
+      final float v0 = part.uv.y() / texScale;
+      final float u1x = (part.uv.x() + part.size.x()) / texScale;
+      final float u1y = (part.uv.x() + part.size.y()) / texScale;
+      final float u1z = (part.uv.x() + part.size.z()) / texScale;
+      final float v1x = (part.uv.y() + part.size.x()) / texScale;
+      final float v1y = (part.uv.y() + part.size.y()) / texScale;
+      final float v1z = (part.uv.y() + part.size.z()) / texScale;
 
       // North
       BakedQuadBuilder builder = new BakedQuadBuilder(sprite);
-      this.putVertexData(builder, new Vector4f(x0, y0, z0, 1.0f), new Vec2(0, 0), Direction.NORTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x0, y1, z0, 1.0f), new Vec2(0, 1), Direction.NORTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x1, y1, z0, 1.0f), new Vec2(1, 1), Direction.NORTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x1, y0, z0, 1.0f), new Vec2(1, 0), Direction.NORTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y0, z0, 1.0f), new Vec2(u1z, v1z), Direction.NORTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y1, z0, 1.0f), new Vec2(u1z, v0 ), Direction.NORTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y1, z0, 1.0f), new Vec2(u0 , v0 ), Direction.NORTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y0, z0, 1.0f), new Vec2(u0 , v1z), Direction.NORTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
       builder.setQuadOrientation(Direction.NORTH);
       modelBuilder.addGeneralQuad(builder.build());
 
       // South
       builder = new BakedQuadBuilder(sprite);
-      this.putVertexData(builder, new Vector4f(x0, y0, z1, 1.0f), new Vec2(0, 0), Direction.SOUTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x1, y0, z1, 1.0f), new Vec2(1, 0), Direction.SOUTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x1, y1, z1, 1.0f), new Vec2(1, 1), Direction.SOUTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x0, y1, z1, 1.0f), new Vec2(0, 1), Direction.SOUTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y0, z1, 1.0f), new Vec2(u0 , v1z), Direction.SOUTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y0, z1, 1.0f), new Vec2(u1z, v1z), Direction.SOUTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y1, z1, 1.0f), new Vec2(u1z, v0 ), Direction.SOUTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y1, z1, 1.0f), new Vec2(u0 , v0 ), Direction.SOUTH.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
       builder.setQuadOrientation(Direction.SOUTH);
       modelBuilder.addGeneralQuad(builder.build());
 
       // West
       builder = new BakedQuadBuilder(sprite);
-      this.putVertexData(builder, new Vector4f(x0, y0, z0, 1.0f), new Vec2(0, 0), Direction.WEST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x0, y0, z1, 1.0f), new Vec2(1, 0), Direction.WEST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x0, y1, z1, 1.0f), new Vec2(1, 1), Direction.WEST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x0, y1, z0, 1.0f), new Vec2(0, 1), Direction.WEST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y0, z0, 1.0f), new Vec2(u0 , v1x), Direction.WEST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y0, z1, 1.0f), new Vec2(u1x, v1x), Direction.WEST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y1, z1, 1.0f), new Vec2(u1x, v0 ), Direction.WEST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y1, z0, 1.0f), new Vec2(u0 , v0 ), Direction.WEST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
       builder.setQuadOrientation(Direction.WEST);
       modelBuilder.addGeneralQuad(builder.build());
 
       // East
       builder = new BakedQuadBuilder(sprite);
-      this.putVertexData(builder, new Vector4f(x1, y0, z0, 1.0f), new Vec2(0, 0), Direction.EAST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x1, y1, z0, 1.0f), new Vec2(0, 1), Direction.EAST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x1, y1, z1, 1.0f), new Vec2(1, 1), Direction.EAST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x1, y0, z1, 1.0f), new Vec2(1, 0), Direction.EAST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y0, z0, 1.0f), new Vec2(u1x, v1x), Direction.EAST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y1, z0, 1.0f), new Vec2(u1x, v0 ), Direction.EAST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y1, z1, 1.0f), new Vec2(u0 , v0 ), Direction.EAST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y0, z1, 1.0f), new Vec2(u0 , v1x), Direction.EAST.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
       builder.setQuadOrientation(Direction.EAST);
       modelBuilder.addGeneralQuad(builder.build());
 
       // Up
       builder = new BakedQuadBuilder(sprite);
-      this.putVertexData(builder, new Vector4f(x0, y1, z0, 1.0f), new Vec2(0, 0), Direction.UP.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x0, y1, z1, 1.0f), new Vec2(1, 0), Direction.UP.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x1, y1, z1, 1.0f), new Vec2(1, 1), Direction.UP.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x1, y1, z0, 1.0f), new Vec2(0, 1), Direction.UP.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y1, z0, 1.0f), new Vec2(u0 , v0 ), Direction.UP.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y1, z1, 1.0f), new Vec2(u0 , v1y), Direction.UP.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y1, z1, 1.0f), new Vec2(u1y, v1y), Direction.UP.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y1, z0, 1.0f), new Vec2(u1y, v0 ), Direction.UP.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
       builder.setQuadOrientation(Direction.UP);
       modelBuilder.addGeneralQuad(builder.build());
 
       // Down
       builder = new BakedQuadBuilder(sprite);
-      this.putVertexData(builder, new Vector4f(x0, y0, z0, 1.0f), new Vec2(0, 0), Direction.DOWN.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x1, y0, z0, 1.0f), new Vec2(0, 1), Direction.DOWN.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x1, y0, z1, 1.0f), new Vec2(1, 1), Direction.DOWN.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-      this.putVertexData(builder, new Vector4f(x0, y0, z1, 1.0f), new Vec2(1, 0), Direction.DOWN.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y0, z0, 1.0f), new Vec2(u0 , v1y), Direction.DOWN.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y0, z0, 1.0f), new Vec2(u1y, v1y), Direction.DOWN.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x1, y0, z1, 1.0f), new Vec2(u1y, v0 ), Direction.DOWN.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
+      this.putVertexData(builder, new Vector4f(x0, y0, z1, 1.0f), new Vec2(u0 , v0 ), Direction.DOWN.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
       builder.setQuadOrientation(Direction.DOWN);
       modelBuilder.addGeneralQuad(builder.build());
-
-//      for(final Direction direction : Direction.values()) {
-//        final BakedQuadBuilder builder = new BakedQuadBuilder(sprite);
-//
-//        final float x0 = x / scale;
-//        final float x1 = (x + part.size.x() * (direction.getAxis() != Direction.Axis.X ? direction.getAxisDirection().getStep() : 0)) / scale;
-//        final float y0 = y / scale;
-//        final float y1 = (y + part.size.y() * (direction.getAxis() != Direction.Axis.Y ? direction.getAxisDirection().getStep() : 0)) / scale;
-//        final float z0 = z / scale;
-//        final float z1 = (z + part.size.z() * (direction.getAxis() != Direction.Axis.Z ? direction.getAxisDirection().getStep() : 0)) / scale;
-//
-//        this.putVertexData(builder, new Vector4f(x0, y0, z1, 1), new Vec2(0, 0), direction.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-//        this.putVertexData(builder, new Vector4f(x1, y0, z0, 1), new Vec2(1, 0), direction.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-//        this.putVertexData(builder, new Vector4f(x1, y1, z0, 1), new Vec2(1, 1), direction.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-//        this.putVertexData(builder, new Vector4f(x0, y1, z1, 1), new Vec2(0, 1), direction.getNormal(), new Vector4f(1, 1, 1, 1), new Vec2(0, 0), sprite);
-//
-//        builder.setQuadOrientation(direction);
-//
-//        modelBuilder.addGeneralQuad(builder.build());
-//      }
-
-      for(final Part part1 : part.children.values()) {
-        this.buildPart(modelBuilder, sprite, part1, x, y, z);
-      }
     }
 
     private void putVertexData(IVertexConsumer consumer, Vector4f position0, Vec2 texCoord0, Vec3i normal0, Vector4f color0, Vec2 uv2, TextureAtlasSprite texture) {
@@ -238,24 +232,16 @@ public class CsjsModel implements IMultipartModelGeometry<CsjsModel> {
       final JsonArray tree = GsonHelper.getAsJsonArray(json, "tree", null);
       Objects.requireNonNull(tree);
 
-      final Map<String, Part> parts = this.loadParts(tree, new HashMap<>());
+      final Map<String, Part> parts = this.loadParts(tree, new HashMap<>(), new Vector3f());
 
       return new CsjsModel(title, parts);
     }
 
-    private Map<String, Part> loadParts(final JsonArray jsonArray, final Map<String, Part> parts) {
+    private Map<String, Part> loadParts(final JsonArray jsonArray, final Map<String, Part> parts, final Vector3f parentPos) {
       for(final JsonElement element : jsonArray) {
         final JsonObject json = element.getAsJsonObject();
 
         final String name = GsonHelper.getAsString(json, "name");
-        final Map<String, Part> children = new HashMap<>();
-
-        if(json.has("children")) {
-          final JsonArray childrenJson = GsonHelper.getAsJsonArray(json, "children", null);
-          Objects.requireNonNull(childrenJson);
-
-          this.loadParts(childrenJson, children);
-        }
 
         final Vector3f pos = this.parseVec3(GsonHelper.getAsJsonArray(json, "position"));
         final Vector3f offset = this.parseVec3(GsonHelper.getAsJsonArray(json, "offsetFromPivot"));
@@ -263,7 +249,19 @@ public class CsjsModel implements IMultipartModelGeometry<CsjsModel> {
         final Vector3f rotation = this.parseVec3(GsonHelper.getAsJsonArray(json, "rotation"));
         final Vector3f uv = this.parseVec2(GsonHelper.getAsJsonArray(json, "texOffset"));
 
-        parts.put(name, new Part(name, children, pos, offset, size, rotation, uv));
+        final Map<String, Part> children = new HashMap<>();
+
+        if(json.has("children")) {
+          final JsonArray childrenJson = Objects.requireNonNull(GsonHelper.getAsJsonArray(json, "children", null));
+
+          final float parentX = parentPos.x() + pos.x() + offset.x();
+          final float parentY = parentPos.y() + pos.y() + offset.y();
+          final float parentZ = parentPos.z() + pos.z() + offset.z();
+
+          this.loadParts(childrenJson, children, new Vector3f(parentX, parentY, parentZ));
+        }
+
+        parts.put(name, new Part(name, children, parentPos, pos, offset, size, rotation, uv));
       }
 
       return parts;
